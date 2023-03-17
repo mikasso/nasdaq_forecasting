844ccc2b9cbb6fbbd356b36661774d51b89002a4
mikasso <mikolaj.szaroleta99@gmail.com>
2023-03-17 12:04:13 +0100

    add data loader, multiple series training

--- lstm.py
+++ lstm.py
@@ -1,84 +1,83 @@
-from torchmetrics import MeanAbsolutePercentageError, MeanSquaredError
+from torchmetrics import MeanSquaredError, MeanAbsolutePercentageError
 import const as CONST
 
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import numpy as np
-import pandas as pd
-import shutil
-from sklearn.preprocessing import MinMaxScaler
-from tqdm import tqdm_notebook as tqdm
 
 import matplotlib.pyplot as plt
 
 from darts import TimeSeries
 from darts.dataprocessing.transformers import Scaler
-from darts.models import RNNModel, ExponentialSmoothing, BlockRNNModel
+from darts.models import RNNModel
 from darts.metrics import mape
-from darts.utils.statistics import check_seasonality, plot_acf
-from darts.datasets import AirPassengersDataset, SunspotsDataset
-from darts.utils.timeseries_generation import datetime_attribute_timeseries
-from pytorch_lightning.callbacks import EarlyStopping
-import warnings
+from pytorch_lightning.callbacks import EarlyStopping, BatchSizeFinder, LearningRateFinder
+from pytorch_lightning.loggers import TensorBoardLogger
 
-from utils import load_dataset, read_csv_ts
+from utils import SeqDataset
 
 torch.set_float32_matmul_precision("medium")
-df = read_csv_ts(f"{CONST.PATHS.MERGED}/AEM.csv")
-series = TimeSeries.from_dataframe(df)
-train, val = series.split_before(CONST.TRAIN_DATE_SPLIT)
-test_input, test = val.split_before(CONST.VAL_DATE_SPLIT)
 
-transformer = Scaler()
-train_transformed = transformer.fit_transform(train)
-val_transformed = transformer.transform(val)
-test_transformed = transformer.transform(test)
-test_input_transformed = transformer.transform(test_input)
-series_transformed = transformer.transform(series)
+
+dataset = SeqDataset(sanity_check=True)
 
 my_stopper = EarlyStopping(
     monitor="val_loss",
-    patience=5,
-    min_delta=0.001,
+    patience=20,
+    min_delta=0.1,
     mode="min",
 )
 my_model = RNNModel(
     model="LSTM",
-    hidden_dim=40,
-    n_rnn_layers=3,
+    hidden_dim=150,
+    n_rnn_layers=12,
     dropout=0.2,
     batch_size=128,
     n_epochs=400,
     optimizer_kwargs={"lr": 1e-3},
-    model_name="AEM_RNN",
+    model_name="LSTM_SEQ",
     log_tensorboard=True,
     random_state=42,
-    training_length=15 * 22,
-    input_chunk_length=15 * 20,
+    input_chunk_length=15 * 10,
+    output_chunk_length=15 * 1,
     force_reset=True,
     save_checkpoints=True,
-    pl_trainer_kwargs={"callbacks": [my_stopper], "accelerator": "gpu", "devices": [0]},
-    loss_fn=MeanSquaredError(),
+    pl_trainer_kwargs={
+        "logger": TensorBoardLogger(save_dir="darts_logs/LSTM_SEQ/logs", name="", version="logs"),
+        "callbacks": [my_stopper, LearningRateFinder()],
+        "accelerator": "gpu",
+        "devices": [0],
+    },
+    loss_fn=MeanAbsolutePercentageError(),
 )
 
 my_model.fit(
-    train_transformed,
-    val_series=val_transformed,
+    dataset.train_transformed,
+    val_series=dataset.val_transformed,
     verbose=True,
 )
 
 
-def eval_model(model, expected_series):
-    pred_series = model.predict(n=len(expected_series), series=test_input_transformed)
-    plt.figure(figsize=(8, 5))
-    transformer.inverse_transform(series_transformed)["price"].plot(label="historical")
-    transformer.inverse_transform(pred_series)["price"].plot(label="forecast")
-    plt.title("MAPE: {:.2f}%".format(mape(pred_series["price"], expected_series["price"])))
-    plt.legend()
+def eval_model(model, dataset):
+    scores = {}
+    for idx, ticker in enumerate(CONST.TICKERS):
+        expected_output_series = dataset.test_seq[idx]["price"]
+        original_series = dataset.series_seq[idx]["price"]
+
+        input_series_transformed = dataset.test_input_transformed[idx]
+        output_series_tranformed = model.predict(n=len(expected_output_series), series=input_series_transformed)
+        output_series = dataset.transformer.inverse_transform(output_series_tranformed)["price"]
+        scores[ticker] = mape(output_series, expected_output_series)
+
+        plt.figure(figsize=(8, 5))
+        original_series.plot(label="original")
+        output_series.plot(label="forecast")
+        plt.title(ticker + " - MAPE: {:.2f}%".format(scores[ticker]))
+        plt.legend()
+        plt.show()
+
+    return scores
 
 
-eval_model(my_model, test_transformed)
+eval_model(my_model, dataset)
 
-best_model = RNNModel.load_from_checkpoint(model_name="AEM_RNN", best=True)
-eval_model(best_model, test_transformed)
+best_model = RNNModel.load_from_checkpoint(model_name="LSTM_SEQ", best=True)
+eval_model(best_model, dataset)
--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
+from typing import List, Tuple
 import numpy as np
 import pandas as pd
 import const as CONST
 from const import FEATURES
+from darts.dataprocessing.transformers import Scaler
+from darts import TimeSeries
 
 
 def read_csv_ts(csv_path: str) -> pd.DataFrame:
@@ -15,8 +18,177 @@ def robust_pct(df: pd.DataFrame) -> pd.DataFrame:
     return df.pct_change().replace([np.inf, -np.inf], np.nan).dropna()
 
 
-def load_dataset() -> pd.DataFrame:
-    dfs = []
-    for ticker in CONST.TICKERS:
-        dfs.append(pd.read_csv(f"{CONST.PATHS.MERGED}/{ticker}.csv"))
-    return pd.concat(dfs, axis="column")
+class SeqDataset:
+    def __init__(self, sanity_check=False) -> None:
+        self._series_seq, self._train_seq, self._val_seq, self._test_seq = [], [], [], []
+        if sanity_check == True:
+            print("Loading data for sanity check")
+
+        for ticker in CONST.TICKERS:
+            if sanity_check == False:
+                df = read_csv_ts(f"{CONST.PATHS.MERGED}/{ticker}.csv")
+                series = TimeSeries.from_dataframe(df)
+                train, rest = series.split_before(CONST.TRAIN_DATE_SPLIT)
+                val, test = rest.split_before(CONST.VAL_DATE_SPLIT)
+            else:
+                df = read_csv_ts(f"{CONST.PATHS.MERGED}/{ticker}.csv")[:1000]
+                series = TimeSeries.from_dataframe(df)
+                train, rest = series.split_before(800)
+                val, test = rest.split_before(100)
+
+            self._series_seq.append(series)
+            self._train_seq.append(train)
+            self._val_seq.append(val)
+            self._test_seq.append(test)
+
+        self._transformer = Scaler()
+        self._train_transformed = self._transformer.fit_transform(self._train_seq)
+        self._val_transformed = self._transformer.transform(self._val_seq)
+        self._test_transformed = self._transformer.transform(self._test_seq)
+        self._series_transformed = self._transformer.transform(self._series_seq)
+
+    @property
+    def series_seq(self) -> List[TimeSeries]:
+        return self._series_seq
+
+    @property
+    def train_seq(self) -> List[TimeSeries]:
+        return self._train_seq
+
+    @property
+    def val_seq(self) -> List[TimeSeries]:
+        return self._val_seq
+
+    @property
+    def test_seq(self) -> List[TimeSeries]:
+        return self._test_seq
+
+    @property
+    def test_input(self) -> List[TimeSeries]:
+        return self._concatanete_seq(self.train_seq, self.val_seq)
+
+    # TRANSFORMED SETS
+
+    @property
+    def series_transformed(self) -> List[TimeSeries]:
+        return self._series_transformed
+
+    @property
+    def train_transformed(self) -> List[TimeSeries]:
+        return self._train_transformed
+
+    @property
+    def val_transformed(self) -> List[TimeSeries]:
+        return self._val_transformed
+
+    @property
+    def test_transformed(self) -> List[TimeSeries]:
+        return self._test_transformed
+
+    @property
+    def test_input_transformed(self) -> List[TimeSeries]:
+        return self._concatanete_seq(self.train_transformed, self.val_transformed)
+
+    @property
+    def transformer(self) -> Scaler:
+        return self._transformer
+
+    def _concatanete_seq(self, a: List[TimeSeries], b: List[TimeSeries]) -> List[TimeSeries]:
+        return list(map(lambda xy: xy[0].concatenate(xy[1]), zip(a, b)))
+
+
+class SeqDatasetWithCov:
+    def __init__(self, sanity_check=False) -> None:
+        self.dataset = SeqDataset(sanity_check)
+
+    def _map_feature(self, seq: List[TimeSeries], feature: str) -> List[TimeSeries]:
+        return list(map(lambda s: s[feature], seq))
+
+    def _concatanete_seq(self, a: List[TimeSeries], b: List[TimeSeries]) -> List[TimeSeries]:
+        return list(map(lambda xy: xy[0].concatenate(xy[1]), zip(a, b)))
+
+    @property
+    def series_seq(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.series_seq, CONST.FEATURES.PRICE)
+
+    @property
+    def train_seq(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.train_seq, CONST.FEATURES.PRICE)
+
+    @property
+    def val_seq(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.val_seq, CONST.FEATURES.PRICE)
+
+    @property
+    def test_seq(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.test_seq, CONST.FEATURES.PRICE)
+
+    @property
+    def test_input(self) -> List[TimeSeries]:
+        return self._concatanete_seq(self.train_seq, self.val_seq)
+
+    # COVARIATES
+
+    @property
+    def series_seq_cov(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.series_seq, CONST.FEATURES.SHARES)
+
+    @property
+    def train_seq_cov(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.train_seq, CONST.FEATURES.SHARES)
+
+    @property
+    def val_seq_cov(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.val_seq, CONST.FEATURES.SHARES)
+
+    @property
+    def test_seq_cov(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.test_seq, CONST.FEATURES.SHARES)
+
+    @property
+    def test_input_cov(self) -> List[TimeSeries]:
+        return self._concatanete_seq(self.train_seq_cov, self.val_seq_cov)
+
+    # TRANSFORMED SETS
+
+    @property
+    def series_transformed(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.series_transformed, CONST.FEATURES.PRICE)
+
+    @property
+    def train_transformed(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.train_transformed, CONST.FEATURES.PRICE)
+
+    @property
+    def val_transformed(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.val_transformed, CONST.FEATURES.PRICE)
+
+    @property
+    def test_transformed(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.test_transformed, CONST.FEATURES.PRICE)
+
+    @property
+    def test_input_transformed(self) -> List[TimeSeries]:
+        return self._concatanete_seq(self.train_transformed, self.val_transformed)
+
+    # TRANSFORMED COVARIATES
+
+    @property
+    def series_transformed_cov(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.series_transformed, CONST.FEATURES.SHARES)
+
+    @property
+    def train_transformed_cov(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.train_transformed, CONST.FEATURES.SHARES)
+
+    @property
+    def val_transformed_cov(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.val_transformed, CONST.FEATURES.SHARES)
+
+    @property
+    def test_transformed_cov(self) -> List[TimeSeries]:
+        return self._map_feature(self.dataset.test_transformed, CONST.FEATURES.SHARES)
+
+    @property
+    def test_input_transformed_cov(self) -> List[TimeSeries]:
+        return self._concatanete_seq(self.train_transformed_cov, self.val_transformed_cov)
